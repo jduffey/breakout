2d5e8c16bb87b12c8f285c6b7a23aef01ce667b3
Jed Duffey <jedduffey@gmail.com>
2018-07-10 21:24:22 -0400

    Add graphics properties; add methods in constructor; HAS ERROR

--- src/main/java/com/jedduffey/breakout/Gameplay.java
+++ src/main/java/com/jedduffey/breakout/Gameplay.java
@@ -1,12 +1,59 @@
 package com.jedduffey.breakout;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Gameplay extends JPanel implements KeyListener, ActionListener {
+
+    private boolean play = false;
+    private int score = 0;
+
+    private int totalBricks = 21;
+
+    private Timer time;
+    private int delay = 8;
+
+    private int playerX = 310;
+
+    private int ballPosX = 120;
+    private int ballPoxY = 350;
+    private int ballXDir = -1;
+    private int ballYDir = -2;
+
+    public Gameplay() {
+
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+
+    public void paint(Graphics g) {
+
+        // background
+        g.setColor(Color.black);
+        g.fillRect(1, 1, 692, 592);
+
+        // borders
+        g.setColor(Color.yellow);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(0, 0, 3, 592);
+
+        // paddle
+        g.setColor(Color.green);
+        g.fillRect(playerX, 550, 100, 8);
+
+        // ball
+        g.setColor(Color.yellow);
+        g.fillRect(ballPosX, ballPoxY, 20, 20);
+
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
