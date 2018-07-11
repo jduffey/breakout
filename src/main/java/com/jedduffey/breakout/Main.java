1ee88ec8666b1df52753f9a82719b349fdc302f3
Jed Duffey <jedduffey@gmail.com>
2018-07-10 23:00:58 -0400

    Refactor constants

--- src/main/java/com/jedduffey/breakout/Main.java
+++ src/main/java/com/jedduffey/breakout/Main.java
@@ -4,15 +4,21 @@ import javax.swing.*;
 
 public class Main {
 
+    public static final int FRAME_WIDTH = 700;
+    public static final int FRAME_HEIGHT = 600;
+    public static final int FRAME_TOP_LEFT_X_POS = 100;
+    public static final int FRAME_TOP_LEFT_Y_POS = 100;
+    public static final String FRAME_TITLE = "Breakout Ball";
+
     public static void main(String[] args) {
 
-        JFrame obj = new JFrame();
+        JFrame jFrame = new JFrame();
         Gameplay gamePlay = new Gameplay();
-        obj.setBounds(10, 10, 700, 600);
-        obj.setTitle("Breakout Ball");
-        obj.setResizable(false);
-        obj.setVisible(true);
-        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        obj.add(gamePlay);
+        jFrame.setBounds(FRAME_TOP_LEFT_X_POS, FRAME_TOP_LEFT_Y_POS, FRAME_WIDTH, FRAME_HEIGHT);
+        jFrame.setTitle(FRAME_TITLE);
+        jFrame.setResizable(false);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.add(gamePlay);
     }
 }
