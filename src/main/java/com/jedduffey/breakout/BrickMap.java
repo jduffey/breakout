6b328e4b2d3cd46ecc9f9a84471c0f3c69709ad5
Jed Duffey <jedduffey@gmail.com>
2018-07-14 20:33:57 -0400

    Implement logic to set bricks a different color based on random chance

--- src/main/java/com/jedduffey/breakout/BrickMap.java
+++ src/main/java/com/jedduffey/breakout/BrickMap.java
@@ -4,15 +4,19 @@ import java.awt.*;
 
 class BrickMap {
 
+    private static final double AVG_RATIO_OF_SPECIAL_BRICKS = .25;
     boolean brickMapArray[][];
+    private boolean specialBricksArray[][];
     int brickWidth;
     int brickHeight;
 
     BrickMap(int row, int col) {
 
         brickMapArray = new boolean[row][col];
+        specialBricksArray = new boolean[row][col];
 
         setAllBricksToTrue(brickMapArray);
+        setSpecialBricks(specialBricksArray);
 
         brickWidth = 540 / col;
         brickHeight = 150 / row;
@@ -27,12 +31,26 @@ class BrickMap {
         }
     }
 
+    private void setSpecialBricks(boolean map[][]) {
+
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (Math.random() < AVG_RATIO_OF_SPECIAL_BRICKS) {
+                    map[i][j] = true;
+                }
+            }
+        }
+    }
+
     void draw(Graphics2D g) {
 
         for (int i = 0; i < brickMapArray.length; i++) {
             for (int j = 0; j < brickMapArray[0].length; j++) {
                 if (brickMapArray[i][j]) {
-                    g.setColor(Color.white);
+                    if (specialBricksArray[i][j]) {
+                        g.setColor(Color.yellow);
+                    } else
+                        g.setColor(Color.white);
                     g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
 
                     g.setStroke(new BasicStroke(3));
