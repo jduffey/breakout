9552653a2819e82574eeea3a69ce0239e618365e
Jed Duffey <jedduffey@gmail.com>
2018-07-14 22:06:12 -0400

    Spacing

--- src/main/java/com/jedduffey/breakout/BrickMap.java
+++ src/main/java/com/jedduffey/breakout/BrickMap.java
@@ -50,6 +50,7 @@ class BrickMap {
 
                 g.setColor(brickMapArray[i][j].color);
                 g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
+
                 g.setStroke(new BasicStroke(3));
                 g.setColor(Color.BLACK);
                 g.drawRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
