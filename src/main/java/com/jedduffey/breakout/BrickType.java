2f6b9460df5eda29256849017833a2222b86c8d4
Jed Duffey <jedduffey@gmail.com>
2018-07-17 21:11:43 -0400

    Refactor; extract brick point values as constants

--- src/main/java/com/jedduffey/breakout/BrickType.java
+++ src/main/java/com/jedduffey/breakout/BrickType.java
@@ -5,12 +5,12 @@ import java.awt.*;
 public enum BrickType {
 
     B00(Gameplay.BACKGROUND_COLOR, 0),
-    B01(Color.WHITE, 5),
-    B02(Color.GREEN, 6),
-    B03(Color.CYAN, 7),
-    B04(Color.YELLOW, 8),
-    B05(Color.ORANGE, 9),
-    B06(Color.RED, 10);
+    B01(Color.WHITE, BrickTypePointValueConstants.B01_PTS),
+    B02(Color.GREEN, BrickTypePointValueConstants.B02_PTS),
+    B03(Color.CYAN, BrickTypePointValueConstants.B03_PTS),
+    B04(Color.YELLOW, BrickTypePointValueConstants.B04_PTS),
+    B05(Color.ORANGE, BrickTypePointValueConstants.B05_PTS),
+    B06(Color.RED, BrickTypePointValueConstants.B06_PTS);
 
     public Color color;
     public int pointValue;
@@ -21,4 +21,12 @@ public enum BrickType {
         this.pointValue = pointValue;
     }
 
+    private static class BrickTypePointValueConstants {
+        public static final int B01_PTS = 5;
+        public static final int B02_PTS = 6;
+        public static final int B03_PTS = 7;
+        public static final int B04_PTS = 8;
+        public static final int B05_PTS = 9;
+        public static final int B06_PTS = 10;
+    }
 }
